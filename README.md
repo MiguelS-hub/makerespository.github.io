9189246fd1ea93c2bde41b40dad16a5ebba193d7
Miguel Sanchez <miguels53990@gmail.com>
2020-04-28 16:05:30 -0400

    final piece

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # makerespository.github.io
+hi, please keep in mind that my programs is based in moment.js so if you would actually see if my program runs fine. try changing the value of hour and seting it to military time, from 9-17
--- script.js
+++ script.js
@@ -1,108 +1,60 @@
 $(document).ready(function () {
-  localStorageclear();
+  
   var hour = moment().hour();
+  localStorageclear();
+
   var updatetime = setInterval(function () {
     var today = moment().format('MMMM Do YYYY, h:mm:ss a');
     hour = moment().hour();
     $("#Time").text(today);
-    timevalidation();
-    
 
-  },1000)
-   timevalidation();
+  },1000);
+
+  updateHourly = setInterval(function() {
+    timevalidation();
+    localStorageclear();
+  }, 100000)
+
+  timevalidation();
   
   $(".save-btn").on("click", function () {
     var text = $(this).siblings(".text").val();
-   var time = parseInt($(this).parent().attr("id"));
+    var time = parseInt($(this).parent().attr("id"));
     localStorage.setItem(time, text);
     console.log(localStorage, text);
-  //console.log(hour, time);
-//timevalidation();
-
+    localStorageclear();
   })
 
-  function timevalidation() {
+  function timevalidation() { //you can uncomment the line underneeth if you are not checking my homework from 9-5
+    //var hour = 13 
     $(".text").each(function() {
-    //console.log(this);
-   //
-   var time = parseInt($(this).parent().attr("id")); //hour=15;
-  // console.log(time);
-  // var hour=11;
-   if(time==hour){
-     $(this).addClass("present");
-   }
-   if(time<hour){
-    $(this).addClass("past");
-   }
-   if(time>hour){
-    $(this).addClass("future");
-   }
-      // if (hour > time) {
-      //   $(this).addClass("past");
-      // }
-      // else if (time > hour) {
-      //   $(this).addClass("future");
-      // }
-      // else {
-      //   $(this).removeClass("past");
-      //   $(this).addClass("present");
-      // }
-
-
+      var time = parseInt($(this).parent().attr("id"));
+      if(time == hour){
+        $(this).addClass("present");
+      }
+      if(time < hour){
+        $(this).addClass("past");
+      }
+      if(time > hour){
+        $(this).addClass("future");
+      }
     })
 
   }
  function localStorageclear() {
-  
-   if(hour ==9 && hour==17 ){
-     
-   
-  $("#9 .text").val(localStorage.getItem("9"));
-
-  $("#10 .text").val(localStorage.getItem("10"));
-
-  $("#11 .text").val(localStorage.getItem("11"));
-
-  $("#12 .text").val(localStorage.getItem("12"));
-
-  $("#13 .text").val(localStorage.getItem("13"));
-
-  $("#14 .text").val(localStorage.getItem("14"));
-
-  $("#15 .text").val(localStorage.getItem("15"));
-
-  $("#16 .text").val(localStorage.getItem("16"));
-
-  $("#17 .text").val(localStorage.getItem("17"));
+   if(hour >= 9 | hour <= 17 ){
+    $("#9 .text").val(localStorage.getItem("9"));
+    $("#10 .text").val(localStorage.getItem("10"));
+    $("#11 .text").val(localStorage.getItem("11"));
+    $("#12 .text").val(localStorage.getItem("12"));
+    $("#13 .text").val(localStorage.getItem("13"));
+    $("#14 .text").val(localStorage.getItem("14"));
+    $("#15 .text").val(localStorage.getItem("15"));
+    $("#16 .text").val(localStorage.getItem("16"));
+    $("#17 .text").val(localStorage.getItem("17"));
    }
-   else{
+   else {
     localStorage.clear();
    }
- 
-
  }
-
-  // $(".rowclass").each(function() {
-  //   localStorage.getItem(".text").val();
-  // })
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 })
